1696472af2f378037376407177a5d3bb2809f98d
Michael Harley <m.harley267@gmail.com>
2023-04-17 02:31:32 -0700

    Tailwind and linting changes

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
+
+const Hero = () => {
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute insert-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#9153ff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, My name is Michael, but most people call me{' '}
+            <span className="text-[#915eff]">Em</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a Software Engineer that builds APIs.{' '}
+            <br className="sm:block hidden" />I am a team player and a joy to
+            work with.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  )
+}
+
+export default Hero
